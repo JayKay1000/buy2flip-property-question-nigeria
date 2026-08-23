803049988fe25edf581e5af7cd453df14eda8318
JayKay1000 <korede.adeigbe1@gmail.com>
2026-08-23 16:50:27 +0100

    Add referral bonus details to plans.js

--- src/lib/plans.js
+++ src/lib/plans.js
@@ -13,6 +13,7 @@ export const PLANS = [
       "3-month maturity period",
       "5% expected return",
       "Portfolio progress updates",
+      "2% Direct & 0.05% third party referrer bonus",
       "Full access to referral reward programme",
       "Dedicated relationship officer support"
     ],
